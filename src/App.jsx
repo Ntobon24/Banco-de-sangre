32c828c336244aab83a3e57b75585ffef7b4a887
Nicolas Tobon <nicolastobon242>
2024-10-14 22:46:19 -0500

    componente pagina principal creado

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/header/header'
+import PaginaPrincipal from './components/Pagina-principal/Pagina-principal'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Header/>
+      <PaginaPrincipal/>
     </>
   )
 }
