534c0d67b97d7c15fca0cc89367d4cd3d80f0cad
Nicolas Tobon <nicolastobon242>
2024-10-14 23:05:07 -0500

    correccion de nombres

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/header/header'
 import PaginaPrincipal from './components/Pagina-principal/Pagina-principal'
-import PaginaCampañas from './components/Campañas/Pagina-campañas'
+import PaginaCampañas from './components/Pagina-campañas/Pagina-campañas'
 
 function App() {
   const [count, setCount] = useState(0)
